ddaa8e4a0ecd839985f559c23de66ea133a52b8e
NidheeshJain <nidheeshjain01@gmail.com>
2020-07-08 02:21:46 +0530

    this commit goes with previous one

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,3 +35,47 @@ function openAboutTEDx() {
     <p>TEDx events are non-profit, but may use an admission fee or commercial sponsorship to cover costs. Similarly, speakers are not paid. They must also relinquish the copyrights to their materials, which TED may edit and distribute under a Creative Commons license.
     </p>`
 }
+
+function openCuration(){
+    document.getElementById('curationButton').className = "activeButton";
+    document.getElementById('designButton').className = "inactiveButton";
+    document.getElementById('sponsorshipButton').className = "inactiveButton";
+    document.getElementById('exCommsButton').className = "inactiveButton";
+    document.getElementById('curationContainer').style.display = "flex";
+    document.getElementById('designContainer').style.display = "none";
+    document.getElementById('sponsorshipContainer').style.display = "none";
+    document.getElementById('exCommsContainer').style.display = "none";
+}
+
+function openDesign(){
+    document.getElementById('curationButton').className = "inactiveButton";
+    document.getElementById('designButton').className = "activeButton";
+    document.getElementById('sponsorshipButton').className = "inactiveButton";
+    document.getElementById('exCommsButton').className = "inactiveButton";
+    document.getElementById('curationContainer').style.display = "none";
+    document.getElementById('designContainer').style.display = "flex";
+    document.getElementById('sponsorshipContainer').style.display = "none";
+    document.getElementById('exCommsContainer').style.display = "none";
+}
+
+function openSponsorship(){
+    document.getElementById('curationButton').className = "inactiveButton";
+    document.getElementById('designButton').className = "inactiveButton";
+    document.getElementById('sponsorshipButton').className = "activeButton";
+    document.getElementById('exCommsButton').className = "inactiveButton";
+    document.getElementById('curationContainer').style.display = "none";
+    document.getElementById('designContainer').style.display = "none";
+    document.getElementById('sponsorshipContainer').style.display = "flex";
+    document.getElementById('exCommsContainer').style.display = "none";
+}
+
+function openExComms(){
+    document.getElementById('curationButton').className = "inactiveButton";
+    document.getElementById('designButton').className = "inactiveButton";
+    document.getElementById('sponsorshipButton').className = "inactiveButton";
+    document.getElementById('exCommsButton').className = "activeButton";
+    document.getElementById('curationContainer').style.display = "none";
+    document.getElementById('designContainer').style.display = "none";
+    document.getElementById('sponsorshipContainer').style.display = "none";
+    document.getElementById('exCommsContainer').style.display = "flex";
+}
